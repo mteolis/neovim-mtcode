fad4f30b3a4d5e17709dfda19911bf69a848a0d4
Michael Teolis <michael.teolis@outlook.com>
2023-09-15 21:34:51 -0400

    feat: add lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,24 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr, remap = false})
+end)
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+	'rust_analyzer',
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y'] = cmp.mapping.confirm({ select = true }),
+	['<C-Space'] = cmp.mapping.complete(),
+})
+
+lsp.setup()
