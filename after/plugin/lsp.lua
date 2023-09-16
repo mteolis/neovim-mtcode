95c0b4aad6858a9742f50b9415ab99b770565f6b
Michael Teolis <michael.teolis@outlook.com>
2023-09-15 23:14:27 -0400

    fix: key mappings missing closing tag

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,10 +15,18 @@ lsp.ensure_installed({
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space'] = cmp.mapping.complete(),
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-Space>'] = cmp.mapping.confirm({ select = true }),
+	['<C-y>'] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+    sign_icons = { }
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
 })
 
 lsp.setup()
